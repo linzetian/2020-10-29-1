06af83c4063a33da9d866bfb2b64b69dd0606223
linzetian <65435639+linzetian@users.noreply.github.com>
2020-10-29 19:47:54 +0800

    Add files via upload

---
+++ 2020-10-29-1指针数组（通过数组保存地址）.c
@@ -0,0 +1,13 @@
+//指针数组（通过数组保存地址）
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	int b = 20;
+	int c = 30;
+	int* arr[3] = {&a,&b,&c};
+	int i = 0;
+	for (i = 0; i < 3; i++)
+		printf("%d ",*arr[i]);
+	return 0;
+}
